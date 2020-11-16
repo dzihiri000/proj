daf80c6a81db6d489dc7b7b864aff900dbf7dd52
Jane <example@gmail.com>
2020-11-16 11:38:39 +0200

    first app

--- js/script.js
+++ js/script.js
@@ -1,1 +1,21 @@
 "use strict";
+
+const numberOfFilms = +prompt('How much films have you seen?', '');
+
+const personalMovieDb =  {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false,
+};
+
+const a = prompt('What film did you watch last time?', ''),
+b = prompt('What mark for that film?', ''),
+c = prompt('What film did you watch last time?'),
+d = prompt('What mark for that film?');
+
+personalMovieDb.movies[a] = b;
+personalMovieDb.movies[c] = d;
+
+console.log(personalMovieDb);
